19ac5f440c043270ceb58ff475686fe8a3c8c890
David Tavares <139977517+sayydaviid@users.noreply.github.com>
2023-08-31 19:36:47 -0300

    Add files via upload
    código da aula de prog1 sobre lista

---
+++ dia 31(08)/código trintaeumdeagosto.py
@@ -0,0 +1,12 @@
+import impar as impares
+
+lista = [1,2,3,4,5,6,7]
+
+reversa = impares.lista_reversa(lista)
+print(reversa)
+#aqui volta só os numeros impares 
+imparess = impares.lista_impar(lista)
+print(imparess)
+#a função volta a metade da lista
+metadinha = impares.metade_da_lista(lista)
+print(metadinha)
---
+++ dia 31(08)/impar.py
@@ -0,0 +1,9 @@
+def lista_impar(lista):
+    return lista[::2]
+
+def lista_reversa(lista):
+    return lista[::-1]
+
+def metade_da_lista(meio):
+    meiuca = len(meio) // 2
+    return meio[:meiuca]
