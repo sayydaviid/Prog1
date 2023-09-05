142ffdae3ff393a6a55e45a74bea774423faab63
David Tavares <139977517+sayydaviid@users.noreply.github.com>
2023-09-05 20:10:24 -0300

    Add files via upload
    dia 05/09

---
+++ cincodonove/impar.py
@@ -0,0 +1,19 @@
+def lista_impar(lista):
+    return lista[::2]
+
+def lista_reversa(lista):
+    return lista[::-1]
+
+def metade_da_lista(meio):
+    meiuca = len(meio) // 2
+    return meio[:meiuca]
+
+def soma_lista(lista):
+    total = 0
+    for valor in lista:
+        total = total + valor
+    return total
+#
+def maiorvalor(lista):
+    return max(lista,key=abs)
+
